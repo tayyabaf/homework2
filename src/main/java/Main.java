a96dcfcb3c73e9eb8fe52bebb82369bb06cd807b
tayyabaf <tayyabanaheed@gmail.com>
2022-08-31 20:39:47 -0400

    completed hw

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,36 @@
+
+//Tayyaba Farooq - This is me practicing the Java tutorials
 public class Main {
     public static void main (String[] args) {
-        System.out.println ("hi");
+       //Output and Syntax
+        System.out.println ("This is me practicing syntax");
+        System.out.println (9+8);
+
+        /* Here is  a
+        multi-line comment */
+
+        //Playing around with and printing different types of variables
+        int num1 = 5*5;
+        int num2 = 2+2;
+        int num3 = 10/2;
+        System.out.println("5 times 5 is " + num1 + ", 2 plus 2 is " + num2 + ", and 10 divided by 2 is " + num3 );
+
+        char gender = 'f';
+        System.out.println(gender);
+
+        //Practicing if...else statements
+        int age = 20;
+        if (age>=18){
+            System.out.println("I am an adult");
+        }else{
+            System.out.println("I am not an adult");
+        }
+
+        //Assigning the same value to multiple variables
+        boolean adult, canDrive;
+        adult = canDrive = true;
+        }
+
     }
-}
+
 
